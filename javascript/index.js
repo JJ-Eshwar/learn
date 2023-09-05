3df313b4938f29b143917d4587ca17f245b4968f
JJ-Eshwar <jjeshwar776@gmail.com>
2023-09-05 20:34:38 +0530

    code is changed

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,28 @@
 // // let word =Number.parseInt(str.slice(2))
 // console.log(typeof word);
 
-const name ='Eshwar';
-let word ='hi'
-console.log( `i am "${word}" hello`);
+// const name ='Eshwar';
+// let word ='hi'
+// console.log( `i am "${word}" hello`);
+let a=Math.random()*100;
+a=Number.parseInt(a)
+let inp;
+score=100;
+while(inp!=a){
+    score=score-1;
+    inp=prompt("Enter the number:");
+    if(inp==a){
+        console.log("congratulations")
+        console.log(`you guess the actual number in ${100-score} chances`)
+    }
+    else if(inp>a && inp<100){
+     console.log("your number is greater than the actual number.")
+    }
+    else if(inp<a && inp>0){
+     console.log("your number is smaller than the actual number.")
+    }
+    else {
+     console.log("Enter number between 1 to 100.")
+    }
 
+}
